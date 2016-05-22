43d7c8c5c03a07e6cc2626a3fdce87abc8597a88
Shafaiatul Islam <shafaiatul@gmail.com>
2016-05-21 23:11:00 -0400

    Set Properties and Chainable Methods

--- Greetr.js
+++ Greetr.js
@@ -1,13 +1,88 @@
 (function(global, $) {
-
+    // ***************************************
+    // Step 1 => $G point to 'Greetr' function
+    // and it returns a new Greetr.init
+    // ***************************************
     var Greetr = function(firstname, lastname, language) {
         return new Greetr.init(firstname, lastname, language);
     };
 
-    Greetr.prototype = {};
+    var supportedLangs = ['en', 'es'];
 
+    var greetings = {
+        en : 'Hello',
+        es : 'Hola'
+    };
+
+
+    var formalGreetings = {
+        en : 'Greetings',
+        es : 'Saludos'
+    };
+
+    var logMessages = {
+        en : 'Logged in',
+        es : 'Inicio sesion'
+    };
+
+    Greetr.prototype = {
+
+        fullName: function() {
+            return this.firstname + ' ' + this.lastname;
+        },
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstname + '!';
+        },
+
+        formalGreetings: function() {
+            return formalGreetings[this.language] + ', ' + this.fullName();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            //if undefined or null it will be coerced to false
+            if(formal) {
+                msg = this.formalGreetings();
+            } else {
+                msg = this.greeting();
+            }
+
+            if(console) {
+                console.log(msg);
+            }
+            //'this' refers to the calling object at execution time
+            //makes the method chainable
+            return this;
+        },
+
+        log: function() {
+            if(console) {
+                console.log(logMessages[this.language] + ': ' + this.fullName());
+            }
+            return this;
+        },
+
+        setLang : function(lang) {
+            this.language = lang;
+
+            this.validate();
+
+            return this;
+        }
+
+    };
+
+    // ************************************************************
+    // Step 2 => new Greetr.init builds the object, sets the values
+    // ***********************************************************
     Greetr.init = function(firstname, lastname, language){
-        //my 'self' points to the empty object created by the 'new' operator
+        // 'self' points to the empty object created by the 'new' operator
         var self = this;
         self.firstname = firstname || '';
         self.lastname = lastname || '';
